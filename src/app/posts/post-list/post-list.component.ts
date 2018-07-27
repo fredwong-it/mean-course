3a49be8687cfea4a7bc8aab04fe2c49cae0a9ed7
Fred Wong <fredwong.it@gmail.com>
2018-07-26 21:03:45 -0400

    feat: diving into structural directives

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
@@ -8,9 +7,11 @@ import { Component } from '@angular/core';
 })
 
 export class PostListComponent {
-  posts = [
-    { title: 'First Post', content: 'This is the first post\'s content' },
-    { title: 'First Post', content: 'This is the second post\'s content' },
-    { title: 'First Post', content: 'This is the third post\'s content' }
-  ];
+  // posts = [
+  //   { title: 'First Post', content: 'This is the first post\'s content' },
+  //   { title: 'Second Post', content: 'This is the second post\'s content' },
+  //   { title: 'Third Post', content: 'This is the third post\'s content' }
+  // ];
+
+  posts = [];
 }
